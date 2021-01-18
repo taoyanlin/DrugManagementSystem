f18c3f6b9da371d1c507b1d46c4e730dc7ce44c9
admin <123456>
2021-01-18 19:27:44 +0800

    增加配置文件，方便后期修改

--- src/main/java/com/punguin/drugmanagementsystem/controller/UserController.java
+++ src/main/java/com/punguin/drugmanagementsystem/controller/UserController.java
@@ -4,6 +4,7 @@ import com.punguin.drugmanagementsystem.controller.common.BaseController;
 import com.punguin.drugmanagementsystem.entity.User;
 import com.punguin.drugmanagementsystem.entity.common.ResponseResult;
 import com.punguin.drugmanagementsystem.service.UserService;
+import io.lettuce.core.dynamic.annotation.Param;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,7 +27,7 @@ public class UserController extends BaseController {
 
     @ApiOperation("登录接口")
     @RequestMapping("/login")
-    public ResponseResult<User> login(String username,String password){
+    public ResponseResult<User> login(@Param("username") String username,@Param("password") String password){
 
         System.out.println("username:"+username);
         System.out.println("password:"+password);
